e6fe5bd6c97abc5d42c615d89a83e523da28aafd
MuratDevelop <muratabdrasulov01@gmail.com>
2021-09-16 16:02:41 +0300

    commit number1

--- app/intelect_kg/settings.py
+++ app/intelect_kg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6!4v6&bw^gd!sae#d(^c881fj-mbh-w7!+g788cz3mh0p#29u^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
